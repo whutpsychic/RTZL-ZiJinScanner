a9119e39b4575d9e99878c2936575f472698bf3a
zbc <1511828529@qq.com>
2023-06-01 10:47:28 +0800

    ok

--- codesh5/src/main.js
+++ codesh5/src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
 
+// Honeywell PDA EDA61K Android Webview 66.03359.158 fixed up globalThis is not defiend
 import '@ungap/global-this'
 import { createApp } from 'vue'
 import App from './App.vue'
